b8475845b184cd1e60e163ba3f52baa9aa3bee70
AKHIL-882 <51040667+AKHIL-882@users.noreply.github.com>
2022-03-31 17:04:15 +0530

    Add files via upload

---
+++ Updated Files/css/script.js
@@ -0,0 +1,4 @@
+function openPdf() {
+    var omyFrame = document.getElementById("myFrame");
+    omyFrame.style.display = "block";
+    omyFrame.src = "https://www.vssut.ac.in/lecture_notes/lecture1428551440.pdf";}
